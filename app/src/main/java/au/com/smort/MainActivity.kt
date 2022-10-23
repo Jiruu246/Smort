281f9e3eaa44a4a7d0f90237b83fe5774dc4164a
#Jiruu_246 <thinhnguyen042002@gmail.com>
2022-10-23 22:15:50 +1100

    init project

---
+++ app/src/main/java/au/com/smort/MainActivity.kt
@@ -0,0 +1,11 @@
+package au.com.smort
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
